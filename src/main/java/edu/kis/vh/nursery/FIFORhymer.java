5035eaa5f6c6b10f607f403c7f50bdb6e1f8bf4e
Bartosz <228805@edu.p.lodz.pl>
2022-01-05 14:58:48 +0100

    1.5 wygenerowano dokumentacje

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -3,7 +3,11 @@ package edu.kis.vh.nursery;
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
 	public final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
-	
+
+	/***
+	 * metoda zwraca ostatnią liczbe z tablicy numbers
+	 * @return zwracana wartość
+	 */
 	@Override
 	public int countOut() {
 		while (!callCheck())
